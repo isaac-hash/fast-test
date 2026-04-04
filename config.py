278b5ea8d19d1a15bbec959677800f0f3445b702
isaac-hash <zikkychukwudulue@gmail.com>
2026-04-04 23:01:09 +0100

    commit

--- config.py
+++ config.py
@@ -2,7 +2,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     database_url: str
+    port: int = 8000
     
-    model_config = SettingsConfigDict(env_file=".env")
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
 settings = Settings()
